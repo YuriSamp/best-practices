db083db456b1513e8ae6b2f34f1da50edddb424a
YuriSamp <yurisamp123@gmail.com>
2023-07-23 19:59:43 -0300

    fix tsconfig for now

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import dotenv from 'dotenv';
+import { App } from 'octokit';
+import fs from 'fs';
+import http from 'http';
+import { createNodeMiddleware } from '@octokit/webhooks';
+import { prChecker } from './openAi.js';
+
+dotenv.config();
+
+const bestPratices = [
+  'Clean Code',
+  'SOLID',
+  'Functional Programing Best Practices',
+  'OOP Best Practices',
+  'Refactoring book from Martin Fowler',
+];
+
+const appId = process.env.APP_ID as string;
+const privateKeyPath = process.env.PRIVATE_KEY_PATH as string;
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const secret = process.env.WEBHOOK_SECRET as string;
+
+const app = new App({
+  appId,
+  privateKey,
+  webhooks: {
+    secret,
+  },
+});
+
+const message = await prChecker('oi', bestPratices);
+
+async function handlePullRequestOpened({ octokit, payload }) {
+  console.log(
+    `Received a pull request event for #${payload.pull_request.number}`
+  );
+  try {
+    await octokit.request(
+      'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
+      {
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        issue_number: payload.pull_request.number,
+        body: message,
+        headers: {
+          'x-github-api-version': '2022-11-28',
+        },
+      }
+    );
+  } catch (error) {
+    if (error.response) {
+      console.error(
+        `Error! Status: ${error.response.status}. Message: ${error.response.data.message}`
+      );
+    }
+    console.error(error);
+  }
+}
+
+app.webhooks.on('pull_request.opened', handlePullRequestOpened);
+
+app.webhooks.onError((error) => {
+  if (error.name === 'AggregateError') {
+    console.error(`Error processing request: ${error.event}`);
+  } else {
+    console.error(error);
+  }
+});
+
+const port = 3000;
+const host = 'localhost';
+const path = '/api/webhook';
+const localWebhookUrl = `http://${host}:${port}${path}`;
+
+const middleware = createNodeMiddleware(app.webhooks, { path });
+
+http.createServer(middleware).listen(port, () => {
+  console.log(`Server is listening for events at: ${localWebhookUrl}`);
+  console.log('Press Ctrl + C to quit.');
+});
---
+++ src/openAi.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export const prChecker = async (prBody: string, bestPratices: string[]) => {
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo-0301',
+    messages: [
+      {
+        role: 'user',
+        content: `You will act as a high-level code reviewer, you must analyze whether the code follows the best practices ${bestPratices.join(
+          ', '
+        )},
+         you must generate a response using markdown containing two sections: Suggestions for Improvement and Breaking Principles. keep it short, use bullet point, here is the code: ${prBody}`,
+      },
+    ],
+    temperature: 0.1,
+    max_tokens: 1000,
+  });
+
+  if (response.data.choices[0].message) {
+    return response.data.choices[0].message.content;
+  }
+  return 'teste';
+};
