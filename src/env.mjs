fcd949abf2757e3f3fd5f6a5174ffc4847de0f33
YuriSamp <yurisamp123@gmail.com>
2023-07-31 19:40:30 -0300

    supabase minimal setup

---
+++ src/env.mjs
@@ -0,0 +1,29 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    GITHUB_APP_ID: z.string().url(),
+    WEBHOOK_SECRET: z.string().min(1),
+    OPENAI_API_KEY: z.string().min(1),
+    PRIVATE_KEY: z.string().min(1),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+  },
+  client: {
+    GITHUB_APP_ID: z.string().url(),
+    WEBHOOK_SECRET: z.string().min(1),
+    OPENAI_API_KEY: z.string().min(1),
+    PRIVATE_KEY: z.string().min(1),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    GITHUB_APP_ID: process.env.GITHUB_APP_ID,
+    WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+});
