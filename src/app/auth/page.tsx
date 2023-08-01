81111617bf3f82e83eca1699b6972b91715b0cf8
YuriSamp <yurisamp123@gmail.com>
2023-08-01 15:14:28 -0300

    basic github flow

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,38 @@
+'use client'
+
 import React from 'react'
 import { Github } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+
 
 const Auth = () => {
+  const router = useRouter()
+  const supabase = createClientComponentClient()
+
+  async function signInWithGitHub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+
+    console.log({ data, error })
+  }
+
+  async function getSession() {
+    const { data, error } = await supabase.auth.getSession()
+    console.log(data)
+  }
+
   return (
     <main className='w-full min-h-screen flex justify-center items-center'>
-      <div className='flex flex-col justify-center items-center'>
-        <Button>
+      <div className='flex flex-col justify-center items-center gap-3'>
+        <Button onClick={signInWithGitHub}>
           <Github /> Sign in with Github
+        </Button>
+        <Button onClick={getSession}>
+          <Github /> Get Session
         </Button>
       </div>
     </main>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,40 @@
+'use client'
+
 import React from 'react'
+import { Github } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
 
 const Dashboard = () => {
+
+  const supabase = createClientComponentClient()
+  const router = useRouter()
+
+  async function getSession() {
+    const { data, error } = await supabase.auth.getSession()
+    console.log(data)
+  }
+
+  const addOrganization = () => {
+    router.push('https://github.com/apps/pr-quality-checker/installations/select_target')
+  }
+
+
   return (
-    <div>page</div>
+    <main className='w-full min-h-screen flex justify-center items-center'>
+      <div className='flex flex-col justify-center items-center gap-3'>
+        <Button onClick={getSession}>
+          <Github /> Get Session
+        </Button>
+        <Button onClick={addOrganization}>
+          <Github /> Add a organization
+        </Button>
+        <Button onClick={addOrganization}>
+          <Github /> Add a repository
+        </Button>
+      </div>
+    </main>
   )
 }
 
