0c599ad64c017fa0f3323a59e03febd1f739277c
YuriSamp <yurisamp123@gmail.com>
2023-08-04 17:45:38 -0300

    draft for sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,14 @@ import { Github } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
+import principals from '../../principals.json'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Checkbox } from "@/components/ui/checkbox"
 
 const Dashboard = () => {
 
@@ -22,9 +30,33 @@ const Dashboard = () => {
 
 
   return (
-    <main className='w-full min-h-screen flex justify-center items-center'>
-      <div className='flex justify-center gap-10 w-full'>
-        <div className='flex flex-col justify-center items-center gap-3'>
+    <main className='w-full min-h-screen flex  mt-20'>
+      <div className='flex w-full ml-20'>
+        <ul className='min-w-[320px] self-start'>
+          {principals.map(principal => (
+            <div key={principal.id} className='flex gap-5 items-center'>
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className='min-w-[320px]'>{principal.category}</AccordionTrigger>
+                  {principal.bestPractices.map(pratices => (
+                    <AccordionContent key={pratices.id} >
+                      <div className='max-w-[320px] flex flex-row items-center gap-3'>
+                        <Checkbox />
+                        <div>
+                          <h2 className='text-xl'>{pratices.name}</h2>
+                          <p>{pratices.description}</p>
+                        </div>
+                      </div>
+                    </AccordionContent>
+                  ))}
+
+                </AccordionItem>
+              </Accordion>
+            </div>
+          ))
+          }
+        </ul>
+        {/* <div className='flex flex-col justify-center items-center gap-3'>
           <Button onClick={getSession}>
             <Github /> Get Session
           </Button>
@@ -34,7 +66,7 @@ const Dashboard = () => {
           <Button onClick={addOrganization}>
             <Github /> Add a repository
           </Button>
-        </div>
+        </div> */}
       </div>
     </main>
   )
