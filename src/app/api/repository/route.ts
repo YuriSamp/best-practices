fcc627e3695c3e10ef37fcedf020703394fb0f2d
YuriSamp <yurisamp123@gmail.com>
2023-08-05 18:15:56 -0300

    fetch repositories with our app

---
+++ src/app/api/repository/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import { App } from 'octokit';
+
+export async function GET() {
+  const appId = process.env.GITHUB_APP_ID as string;
+  const secret = process.env.WEBHOOK_SECRET as string;
+  //DEV
+  // const privateKey = process.env.PRIVATE_KEY?.replace(/\\n/g, '\n') as string;
+  //PROD
+  const privateKey = process.env.PRIVATE_KEY as string;
+
+  const app = new App({
+    appId,
+    privateKey,
+    webhooks: {
+      secret,
+    },
+  });
+
+  try {
+    const appUsers = await app.octokit.request('GET /app/installations');
+    const octokit = await app.getInstallationOctokit(appUsers.data[1].id);
+    const repositories = await octokit.request(
+      'GET /installation/repositories'
+    );
+    return NextResponse.json({ repositories });
+  } catch (error) {
+    console.log(error);
+  }
+  return new Response(null, {
+    status: 500,
+  });
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,72 +1,84 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Github } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 import principals from '../../principals.json'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import { Avatar } from '@/components/ui/avatar'
 import { Checkbox } from "@/components/ui/checkbox"
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import axios from 'axios'
 
 const Dashboard = () => {
 
-  const supabase = createClientComponentClient()
-  const router = useRouter()
+  const [projects, setProjects] = useState([])
 
-  async function getSession() {
-    const { data, error } = await supabase.auth.getSession()
-    console.log(data)
-  }
+  const router = useRouter()
 
   const addOrganization = () => {
     router.push('https://github.com/apps/pr-quality-checker/installations/select_target')
   }
 
+  const getRepositores = async () => {
+    const repositories = await axios.get('../api/repository')
+    setProjects(repositories.data.repositories.data.repositories)
+  }
+
 
   return (
-    <main className='w-full min-h-screen flex  mt-20'>
-      <div className='flex w-full ml-20'>
-        <ul className='min-w-[320px] self-start'>
+    <main className='flex overflow-y-auto'>
+      <aside className='flex ml-7 '>
+        <ul className='min-w-[320px] h-screen self-start overflow-scroll overflow-x-hidden'>
           {principals.map(principal => (
-            <div key={principal.id} className='flex gap-5 items-center'>
-              <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className='min-w-[320px]'>{principal.category}</AccordionTrigger>
-                  {principal.bestPractices.map(pratices => (
-                    <AccordionContent key={pratices.id} >
-                      <div className='max-w-[320px] flex flex-row items-center gap-3'>
-                        <Checkbox />
-                        <div>
-                          <h2 className='text-xl'>{pratices.name}</h2>
-                          <p>{pratices.description}</p>
-                        </div>
-                      </div>
-                    </AccordionContent>
-                  ))}
-
-                </AccordionItem>
-              </Accordion>
+            <div className='py-5 border-b border-neutral-400' key={principal.id}>
+              <h2 className='min-w-[350px] text-xl font-medium h-12 flex items-center'>{principal.category}</h2>
+              {principal.bestPractices.map(pratices => (
+                <div key={pratices.id} className='min-w-[350px] flex flex-row items-center gap-3'>
+                  <div className='flex gap-3 items-center h-11'>
+                    <Checkbox />
+                    <div>
+                      <h2 className='text-lg' title={pratices.description}>{pratices.name}</h2>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           ))
           }
         </ul>
-        {/* <div className='flex flex-col justify-center items-center gap-3'>
-          <Button onClick={getSession}>
-            <Github /> Get Session
-          </Button>
-          <Button onClick={addOrganization}>
-            <Github /> Add a organization
-          </Button>
-          <Button onClick={addOrganization}>
-            <Github /> Add a repository
-          </Button>
-        </div> */}
+      </aside>
+      <div className='flex flex-col w-full'>
+        <header className='bg-black h-20 w-full flex items-center justify-end px-10'>
+          <div className='flex items-center gap-3'>
+            <span className='text-white text-xl'>Yuri</span>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+
+        <Button onClick={getRepositores}>
+          <Github /> Get repository
+        </Button>
+
+        <section className='mt-20'>
+          <div className='border border-red-500 rounded-sm m-10 h-20 flex justify-end items-center px-5'>
+            <Button onClick={addOrganization}>
+              <Github /> Add a repository
+            </Button>
+          </div>
+        </section>
+        <section className='mt-20'>
+          <ul className='flex flex-col gap-3'>
+            {projects.map(project =>
+            (<li className='text-3xl text-red-400' key={project.id}>
+              {project.name}
+            </li>))}
+          </ul>
+
+        </section>
       </div>
     </main>
   )
