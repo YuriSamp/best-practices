b3abc51c75dccb63007483913ea3378205d3968c
YuriSamp <yurisamp123@gmail.com>
2023-08-06 20:16:59 -0300

    changes supabase client env

--- src/db/getSupabaseClient.ts
+++ src/db/getSupabaseClient.ts
@@ -2,8 +2,8 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js'
 
 export const getSupabaseClient = (): SupabaseClient => {
   const client = createClient(
-    process.env['NEXT_PUBLIC_SUPABASE_URL'] as string,
-    process.env['SUPABASE_SERVICE_ROLE'] as string
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.SUPABASE_SERVICE_ROLE as string
   )
   return client
 }
