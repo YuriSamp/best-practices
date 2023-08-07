e20eda96d0a022d54a9920ed146cc408f3b12788
YuriSamp <yurisamp123@gmail.com>
2023-08-07 19:49:48 -0300

    rewrite webhook

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,17 +1,19 @@
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
 
+const endPointSecret = process.env.STRIPE_PAYMENT_WEBHOOK_SECRET
+
 export async function POST(req: Request) {
   const body = await req.text()
-  const signature = req.headers.get('Stripe-Signature') as string
-  console.log(signature)
+  const signatureHeader = req.headers.get('Stripe-Signature') as string
+  const signature = signatureHeader.replace('[', '').replace(']', '')
   let event: Stripe.Event
 
   try {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
-      process.env.STRIPE_WEBHOOK_SECRET as string
+      endPointSecret as string
     )
   } catch (error: any) {
     return new Response(`Webhook Error: ${error.message}`, { status: 400 })
