06d49c11fcf74cb98d5832974ab8d890f27a0113
YuriSamp <yurisamp123@gmail.com>
2023-08-14 01:06:48 -0300

    add components and rechart

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,24 +2,33 @@
 
 import { getSupabaseClietSide } from '@/lib/supabase'
 import React, { useEffect, useState } from 'react'
+import { Progress } from "@/components/ui/progress"
 
 const Account = () => {
   const supabase = getSupabaseClietSide()
-  const [tokens, setTokens] = useState(0)
+  const [tokens, setTokens] = useState(750)
+  const [maxUsage, setMaxUsage] = useState(10000)
 
 
   useEffect(() => {
-    const getTokens = async () => {
-    }
-    getTokens()
+    (async () => {
+      const { data, error } = await supabase.auth.getUser()
+      const { data: logs, error: logsError } = await supabase.from('Logs').select().eq('user_id', data.user?.id)
+      const { data: maxToknes } = await supabase.from('Users').select().eq('user_uid', data.user?.id)
+      // setMaxUsage(maxToknes?.at(0)?.tokens)
+      // setTokens(logs?.at(0)?.token_count)
+    })()
+
   }, [])
 
 
   return (
     <main className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col'>
-        <span>Tokens utilizados</span>
+        <span>Usage</span>
         <span>{tokens}</span>
+        <span>Max usage : {maxUsage}</span>
+        <Progress value={(tokens % maxUsage) / 100} />
       </div>
     </main>
   )
