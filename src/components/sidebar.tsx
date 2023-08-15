2869d7b22e5d5d9268a112d2ce67eb31a13bf6de
YuriSamp <yurisamp123@gmail.com>
2023-08-15 17:53:26 -0300

    sidebar changes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Folder } from 'lucide-react'
+import { Folder, Plus, LogOut } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React, { Dispatch, SetStateAction } from 'react'
 import { Avatar } from '@/components/ui/avatar'
@@ -6,6 +6,15 @@ import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 import useSessionStore from '@/store/useSessionStore'
 import { BsThreeDots } from 'react-icons/bs'
 import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 type Repository = {
   rules: null | string[]
@@ -40,18 +49,16 @@ const Sidebar = ({ projects, repoSelect, setRepoSelected }: SidebarProps) => {
 
   return (
     <aside className='flex flex-col bg-[#121212] h-screen text-white relative w-80'>
-      <div className='mx-4 mt-24'>
-        <div className='flex justify-start my-4 text-2xl'>
-          <h2>Projects</h2>
-        </div>
+      <div className='mx-4 mt-4'>
         <ul className='w-60 flex flex-col gap-3'>
-          <li className=' rounded-xl  cursor-pointer  bg-[#292524] text-white  '>
-            <button className='h-full w-full flex justify-center px-4 py-5' onClick={addOrganization}>
+          <li className=' rounded-xl  cursor-pointer  text-white  '>
+            <button className='h-full w-full flex justify-between items-center px-4 py-5 border border-[#292524] rounded-xl' onClick={addOrganization}>
+              <Plus className='w-6 h-6' />
               <p className='text-xl'>New project</p>
             </button>
           </li>
           {projects && projects.map(project =>
-          (<li className={`rounded-xl ${repoSelect?.title === project.title ? 'bg-primary' : 'bg-[#292524] '} text-white cursor-pointer flex`} key={project.id}>
+          (<li className={`rounded-xl ${repoSelect?.title === project.title ? 'bg-primary' : 'bg-[#292524] '} text-white cursor-pointer flex hover:bg-primary`} key={project.id}>
             <button className='flex-grow flex justify-between items-center px-4 py-5' onClick={() => handleSelectRepo(project.title)}>
               <Folder />
               <p className='text-xl '>{project.title}</p>
@@ -67,9 +74,26 @@ const Sidebar = ({ projects, repoSelect, setRepoSelected }: SidebarProps) => {
           </Avatar>
           <span className='text-white text-xl'>Yuri</span>
         </div>
-        <Link href={'/account'}>
-          <BsThreeDots className='w-6 h-6 hover:cursor-pointer' />
-        </Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <BsThreeDots className='w-6 h-6 hover:cursor-pointer' />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className='cursor-not-allowed'>Profile</DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={'/account'} className='w-full'>
+                Billing
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className='flex gap-1 cursor-pointer'>
+              <LogOut className='w-5 h-5' />
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
       </div>
     </aside>
   )
