bc5a6973847ae0722853bd0300b23faa277bac97
YuriSamp <yurisamp123@gmail.com>
2023-08-15 19:33:38 -0300

    add accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 import { AiOutlineTool } from 'react-icons/ai'
-import PrExample from '../../public/example.png'
+// import PrExample from '../../public/example.png'
+import Dashboard from '../../public/dashboard.png'
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 export default function Home() {
 
@@ -50,16 +58,69 @@ export default function Home() {
         </Link>
       </section>
       <section className='py-10'>
-        <h2 className='text-center text-3xl py-3'>The most dead-simple tool to improve your pull request</h2>
-        <div className='my-5 flex justify-between gap-10' >
-          <Image src={PrExample} alt='Pull Request example' width={640} className='rounded-xl' />
-          <ol className='flex flex-col gap-20 w-80'>
-            <li className='text-2xl'>How it works</li>
-            <li className='text-xl'>1 install the github app in your repository</li>
-            <li className='text-xl'>2 Save in dashboard what best-practices you want to keep track</li>
-            <li className='text-xl'>3 read the suggested changes and make more changes</li>
-          </ol>
-        </div>
+        <h2 className='text-center text-3xl font-bold py-3'>The most dead-simple tool to improve your pull request</h2>
+        <ol className='flex flex-col gap-10 pt-5'>
+          <li className='my-5 flex justify-between gap-10'>
+            <Image src={Dashboard} alt='Pull Request example' width={600} />
+            <div>
+              <p className='text-2xl font-bold'>Simplicity in Installing the GitHub App</p>
+              <p className='mt-5 text-lg'>Install our GitHub App with just a few clicks. Authorize the permissions and the App will automatically detect the repositories. No complex configurations.</p>
+            </div>
+          </li>
+          <li className='my-5 flex justify-between gap-10' >
+            <div>
+              <p className='text-2xl font-bold'>Simple to keep track your code</p>
+              <p className='mt-5 text-lg'>Easily customize code review rules in our dashboard. Activate, deactivate, or edit guidelines with a user-friendly interface. Our documentation will be available for guidance.</p>
+            </div>
+            <Image src={Dashboard} alt='Pull Request example' width={640} className='rounded-xl' />
+          </li>
+          <li className='my-5 flex justify-between gap-10' >
+            <Image src={Dashboard} alt='Pull Request example' width={640} className='rounded-xl' />
+            <div>
+              <p className='text-2xl font-bold'>Enhancement in Pull Requests:</p>
+              <p className='mt-5 text-lg'>After installation and setup, your pull requests will be automatically evaluated. Receive detailed feedback aligned with the chosen best practices. Effectively improve the quality of your pull requests.</p>
+            </div>
+          </li>
+        </ol>
+      </section>
+      <section className='my-8'>
+        <h2 className='text-center text-3xl font-bold my-3'>Frequently asked questions.</h2>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <p className='text-3xl'>
+                What methods of payment do you accept?
+              </p>
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <p className='text-3xl'>
+                Can I cancel my subscription at any time?
+              </p>
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible className='w-[800px]'>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <p className='text-3xl'>
+                Do you offer refund?
+              </p>
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </section>
       <footer className='flex flex-col  pt-10 mb-3 w-full '>
         <div className='flex w-full my-5 py-5 border-b'>
