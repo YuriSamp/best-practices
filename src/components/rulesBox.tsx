c93257ebe75e99644ba7bd1a158ce87c6185368f
YuriSamp <yurisamp123@gmail.com>
2023-08-20 03:16:45 -0300

    redesign main content

--- src/components/rulesBox.tsx
+++ src/components/rulesBox.tsx
@@ -56,19 +56,16 @@ const RulesBox = ({ repository }: Props) => {
   }
 
   return (
-    <div className='flex justify-center  mt-10 mx-14'>
+    <div className='flex justify-center  mt-10 '>
       <div className='w-1/2 '>
-        <h2 className='text-3xl mb-10 text-center'>Rules to choose</h2>
-        <div className='w-full flex gap-3'>
-          <Input className='bg-background focus:ring-transparent border rounded-lg' />
-          <Button className='bg-primary text-primary-foreground'>Search</Button>
-        </div>
-        <ul className='max-h-[75vh] self-start overflow-scroll overflow-x-hidden mt-10'>
+        <h2 className='text-3xl mb-10 text-center'>Search a practice</h2>
+        <Input className='bg-background focus:ring-transparent border rounded-lg' />
+        <ul className='max-h-[63vh] self-start overflow-scroll overflow-x-hidden mt-10'>
           {principles.map(principle => (
-            <div className='pb-5 mb-5 border-b border-neutral-400' key={principle.id}>
+            <div className='pb-5 mb-5 border-b last:border-0 border-neutral-400 w-full' key={principle.id}>
               <h2 className='w-full text-xl font-medium h-12 flex items-center'>{principle.category}</h2>
               {principle.bestPractices.map(pratices => (
-                <div key={pratices.id} className=' flex flex-row items-center gap-3'>
+                <div key={pratices.id} className={`flex flex-row items-center my-3 w-full`}>
                   <div className='flex gap-3 items-center h-20'>
                     <Checkbox
                       className='border-white'
@@ -92,15 +89,7 @@ const RulesBox = ({ repository }: Props) => {
             </div>
           ))}
         </ul>
-      </div>
-      <div className='flex flex-col items-center w-1/2'>
-        <h2 className='text-3xl mb-10'>Rules selected</h2>
-        <div className='h-4/5'>
-          <ul className='flex flex-wrap gap-4 px-10 w-full max-h-[65vh] overflow-y-scroll justify-center items-center'>
-            {options && options.map((pratices, id) => <li className='px-2 py-2 flex items-center bg-[#292524] text-white text-lg rounded-xl' key={id}>{pratices}</li>)}
-          </ul>
-        </div>
-        <div className='flex gap-5 mt-10'>
+        <div className='flex gap-5 mt-10 items-center justify-center'>
           <Button className='bg-secondary text-secondary-foreground hover:text-primary-foreground w-24 h-16' onClick={handleReset}>Reset</Button>
           <Button className='bg-primary w-24 h-16' onClick={() => handleSaveOptions(repository.title)}>Save</Button>
         </div>
