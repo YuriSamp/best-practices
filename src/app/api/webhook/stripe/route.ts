66c7ab73743d16239f5947690d81ffc2e0c61908
YuriSamp <yurisamp123@gmail.com>
2023-09-04 20:36:04 -0300

    add stripe customer deleted/updated logic

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -43,10 +43,49 @@ export async function POST(req: Request) {
 
   switch (event.type) {
     case 'customer.subscription.deleted': {
-      //TODO Implement latter
+      const subscription = await stripe.subscriptions.retrieve(
+        session.subscription as string
+      )
+
+      const { error } = await supabase
+        .from('Users')
+        .update({
+          stripe_subscription_id: subscription.id,
+          stripe_price_id: subscription.items.data[0].price.id,
+          stripe_current_period_end: String(new Date()),
+          stripe_customer_id: subscription.customer as string,
+          tokens: 0,
+        })
+        .eq('email', session.customer_details?.email)
+
+      if (error) {
+        return new Response(error.message, { status: 500 })
+      }
     }
     case 'customer.subscription.updated': {
-      //TODO Implement latter
+      const subscription = await stripe.subscriptions.retrieve(
+        session.subscription as string
+      )
+
+      let tokens = 0
+      if (subscription.items.data[0].price.unit_amount) {
+        tokens = handleTier(subscription.items.data[0].price.unit_amount)
+      }
+
+      const { error } = await supabase
+        .from('Users')
+        .update({
+          stripe_price_id: subscription.items.data[0].price.id,
+          stripe_current_period_end: String(
+            new Date(subscription.current_period_end * 1000)
+          ),
+          tokens: tokens,
+        })
+        .eq('email', session.customer_details?.email)
+
+      if (error) {
+        return new Response(error.message, { status: 500 })
+      }
     }
     case 'checkout.session.completed': {
       const subscription = await stripe.subscriptions.retrieve(
@@ -80,6 +119,11 @@ export async function POST(req: Request) {
         session.subscription as string
       )
 
+      let tokens = 0
+      if (subscription.items.data[0].price.unit_amount) {
+        tokens = handleTier(subscription.items.data[0].price.unit_amount)
+      }
+
       const { error } = await supabase
         .from('Users')
         .update({
@@ -87,6 +131,7 @@ export async function POST(req: Request) {
           stripe_current_period_end: String(
             new Date(subscription.current_period_end * 1000)
           ),
+          tokens: tokens,
         })
         .eq('stripe_subscription_id', subscription.id)
 
